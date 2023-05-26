a87dfb986ac7bb1d3045d340fbd70c6e74331bb4
shubham pathak <shubhampathak1996@gmail.com>
2023-05-26 17:56:49 +0530

    website_update

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,57 @@ function toggleAccordion() {
 }
 
 items.forEach((item) => item.addEventListener('click', toggleAccordion));
+$('.banners').owlCarousel({
+  loop: true,
+  margin: 20,
+  responsiveClass: true,
+
+  autoplay: true,
+  autoplayTimeout: 3000,
+  autoplayHoverPause: true,
+  nav: true,
+  smartSpeed: 800,
+
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 1,
+    },
+    1000: {
+      items: 1,
+    },
+  },
+});
+
+$('.owl-carousel').owlCarousel({
+  loop: true,
+  margin: 20,
+  responsiveClass: true,
+  autoplayTimeout: 2000,
+  autoplayHoverPause: false,
+  autoplay: true,
+  nav: true,
+  infinite: true,
+  smartSpeed: 250,
+  rewindNav: false,
+  responsiveClass: true,
+
+  autoplay: true,
+
+  responsive: {
+    0: {
+      items: 2,
+      nav: true,
+    },
+    600: {
+      items: 2,
+      nav: false,
+    },
+    1000: {
+      items: 3,
+      nav: true,
+    },
+  },
+});
